ec35f7896700967a0fc2447ddc83892a7c167062
Michel Roberge <michel_roberge@msn.com>
2025-03-12 16:45:50 -0400

    different approach to refrehsing data

--- portfolio.next/src/app/blogs/[slug]/page.tsx
+++ portfolio.next/src/app/blogs/[slug]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { notFound } from "next/navigation";
 import { fetchBlogEntry } from "@/services/blogService";
 import BlogView from "@/components/blog/BlogView";
