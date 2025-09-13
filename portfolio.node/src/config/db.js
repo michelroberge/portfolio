ed3003602081f62b53e4faeb50cc5af13b3aae1c
Michel Roberge <michelroberge77@gmail.com>
2025-09-13 14:53:32 +0000

    fixed server

--- portfolio.node/src/config/db.js
+++ portfolio.node/src/config/db.js
@@ -1,21 +1,5 @@
 const mongoose = require("mongoose");
-const useInMemoryDb = process.env.MONGO_URI !== null;
-
-let connectDB = null;
-if (useInMemoryDb){
-  const { MongoMemoryServer } = require('mongodb-memory-server');
-    mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
-  try {
-    await mongoose.connect(mongoUri);
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Connection Failed", error);
-    process.exit(1);
-  }
-}
-else {
-  connectDB = async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
@@ -24,6 +8,5 @@ else {
     process.exit(1);
   }
 };
-}
 
 module.exports = connectDB;
